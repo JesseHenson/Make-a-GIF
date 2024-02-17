86034d5937f0ca262520fe840a7886339906d49d
Jesse Henson <henson.jhenson.jesse@gmail.com>
2024-02-17 20:55:57 +0000

    Updated from Fork for CX work

--- README.md
+++ README.md
@@ -1,12 +1,36 @@
 
-🤹 Make A GIF 🏄‍♂️
+Welcome to GIF Creator
 
-It is a web application in Python that uses the Streamlit framework and the MoviePy library to convert video files into animated GIF images. 
+# 🌐 Welcome to Web QA
 
-It also enables the user to change a variety of GIF conversion-related settings, including the number of frames per second, the duration of the export, the export speed, etc. 
+### 🏁 Enter your open ai api token, a URL and ask any question you'd like
+---
+### 📨 It uses gpt 3.5 turbo under the hood. It'll even let you know how much it cost you to use. 
 
-![me](https://github.com/SaiSpr/Make-a-GIF/blob/main/Make%20a%20GIF%20using%20Streamlit.gif)
+# 🔧 We Use
+- Beautiful Soup to scrape the website
+- Chromadb as a vectorstore and retriever
+- OpenAI as an embedding agent and LLM
 
-**Credits**
-- Chanin Nantasenamat
+
+
+### For more of my work see [My Portfolio](https://generative-ai-portfolio-daukgnhrbwu5cugphlvjmm.streamlit.app/)
+
+<a href="https://github.com/JesseHenson"  >
+<img alt='Github' src="images/github.png" data-canonical-src="https://gyazo.com/eb5c5741b6a9a16c692170a41a49c858.png" width="80" height="80" />
+</a> 
+&nbsp;
+&nbsp;
+&nbsp;  
+<a href="https://www.linkedin.com/in/jessehensonai" >
+<img alt='LinkedIn' src="images/linkedin.png" width="80" height="80" />
+</a>
+&nbsp;
+&nbsp;
+&nbsp;  
+<a href="https://medium.com/@jesse.henson" >
+<img alt='Medium'  src="images/medium.png" width="80" height="80" />
+</a>
+
+
 
--- streamlit_app.py
+++ streamlit_app.py
@@ -20,54 +20,91 @@ if 'clip_total_frames' not in st.session_state:
     st.session_state.clip_total_frames = 0  
 
 # App title
-st.title('🤹 Make A GIF 🏄‍♂️')
-
-## Upload file ##
-st.sidebar.header('Upload file')
-uploaded_file = st.sidebar.file_uploader("Choose a file", type=['avi','wmv','mov','mp4','mkv'])
+st.title('GIF Creator')
 
 
-# Display GIF parameters once the file is loaded
-if uploaded_file is not None:
+
+def convert_vid(uploaded_file):
   # Saving to temporary file
-  tfile = tempfile.NamedTemporaryFile(delete=False) 
+  file_name, file_extension = os.path.splitext(uploaded_file.name)
+  tfile = tempfile.NamedTemporaryFile(suffix=file_extension, delete=False) 
   tfile.write(uploaded_file.read())
   
   ## Open file ##
-  clip = VideoFileClip(tfile.name)
-    
-  st.session_state.clip_duration = clip.duration
+  st.session_state.clip = VideoFileClip(tfile.name)
+  st.session_state.clip_duration = st.session_state.clip.duration
+
+
+def generate_gif():
+  temp_clip = st.session_state.clip.subclip(st.session_state.selected_export_range[0], 
+                      st.session_state.selected_export_range[1]).speedx(st.session_state.selected_speedx)
+  frames = []
+  for frame in temp_clip.iter_frames():
+      frames.append(np.array(frame))
   
+  image_list = []
+
+  for frame in frames:
+      im = Image.fromarray(frame)
+      image_list.append(im)
+
+  image_list[0].save('export.gif', format = 'GIF', save_all = True, loop = 0, append_images = image_list)
+
+  ## Download ##
+  st.subheader('Download')
+  file_ = open('export.gif', 'rb')
+  contents = file_.read()
+  data_url = base64.b64encode(contents).decode("utf-8")
+  file_.close()
+  st.markdown(
+      f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+      unsafe_allow_html=True,
+  )
+  
+  fsize = round(os.path.getsize('export.gif')/(1024*1024), 1)
+  st.info(f'File size of generated GIF: {fsize} MB', icon='💾')
+  print('Ran Generate GIF')
+
+with st.form('my_form'):
+  ## Upload file ##
+  st.header('Upload file')
+  uploaded_file = st.file_uploader("Choose a file", type=['avi','wmv','mov','mp4','mkv'])
+  submitted = st.form_submit_button('Submit')
+  if submitted:
+      convert_vid(uploaded_file)
+
+# Display GIF parameters once the file is loaded
+if 'clip' in st.session_state: 
   # Sidebar widgets
   st.sidebar.header('Input parameters')
-  selected_resolution_scaling = st.sidebar.slider('Scaling of video resolution', 0.0, 2.0, 0.5 )
-  selected_speedx = st.sidebar.slider('Playback speed', 0.1, 10.0, 2.0)
-  selected_export_range = st.sidebar.slider('Duration range to export in seconds', 0, int(st.session_state.clip_duration), (0, int(st.session_state.clip_duration) ))
-    
+  st.session_state.selected_resolution_scaling = st.sidebar.slider('Scaling of video resolution', 0.0, 2.0, 0.5 )
+  st.session_state.selected_speedx = st.sidebar.slider('Playback speed', 0.1, 10.0, 2.0)
+  st.session_state.selected_export_range = st.sidebar.slider('Duration range to export in seconds', 0, int(st.session_state.clip_duration), (0, int(st.session_state.clip_duration) ))
+
   ## Resizing of video
-  clip = clip.resize(selected_resolution_scaling)
-     
-  st.session_state.clip_width = clip.w
-  st.session_state.clip_height = clip.h
-  st.session_state.clip_duration = clip.duration
-  st.session_state.clip_total_frames = clip.duration * clip.fps
-  st.session_state.clip_fps = st.sidebar.slider('FPS', 10, 60, 20)
+  tmp_clip = st.session_state.clip.resize(st.session_state.selected_resolution_scaling)
+      
+  clip_width = tmp_clip.w
+  clip_height = tmp_clip.h
+  clip_duration = tmp_clip.duration
+  clip_total_frames = tmp_clip.duration * tmp_clip.fps
+  clip_fps = st.sidebar.slider('FPS', 10, 60, 20)
     
   ## Display output
   st.subheader('Metrics')
   col1, col2, col3, col4, col5 = st.columns(5)
-  col1.metric('Width', st.session_state.clip_width, 'pixels')
-  col2.metric('Height', st.session_state.clip_height, 'pixels')
-  col3.metric('Duration', st.session_state.clip_duration, 'seconds')
-  col4.metric('FPS', st.session_state.clip_fps, '')
-  col5.metric('Total Frames', st.session_state.clip_total_frames, 'frames')
+  col1.metric('Width', clip_width, 'pixels')
+  col2.metric('Height', clip_height, 'pixels')
+  col3.metric('Duration', clip_duration, 'seconds')
+  col4.metric('FPS', clip_fps, '')
+  col5.metric('Total Frames', clip_total_frames, 'frames')
+
 
   # Extract video frame as a display image
   st.subheader('Preview')
-
   with st.expander('Show image'):
     selected_frame = st.slider('Preview a time frame (s)', 0, int(st.session_state.clip_duration), int(np.median(st.session_state.clip_duration)) )
-    clip.save_frame('frame.gif', t=selected_frame)
+    tmp_clip.save_frame('frame.gif', t=selected_frame)
     frame_image = Image.open('frame.gif')
     st.image(frame_image)
 
@@ -76,60 +113,27 @@ if uploaded_file is not None:
   with st.expander('Show image parameters'):
     st.write(f'File name: `{uploaded_file.name}`')
     st.write('Image size:', frame_image.size)
-    st.write('Video resolution scaling', selected_resolution_scaling)
-    st.write('Speed playback:', selected_speedx)
-    st.write('Export duration:', selected_export_range)
+    st.write('Video resolution scaling', st.session_state.selected_resolution_scaling)
+    st.write('Speed playback:', st.session_state.selected_speedx)
+    st.write('Export duration:', st.session_state.selected_export_range)
     st.write('Frames per second (FPS):', st.session_state.clip_fps)
-    
-  ## Export animated GIF ##
-  st.subheader('Generate GIF')
-  generate_gif = st.button('Generate Animated GIF')
   
-  if generate_gif:
-    clip = clip.subclip(selected_export_range[0], selected_export_range[1]).speedx(selected_speedx)
-    
-    frames = []
-    for frame in clip.iter_frames():
-        frames.append(np.array(frame))
-    
-    image_list = []
 
-    for frame in frames:
-        im = Image.fromarray(frame)
-        image_list.append(im)
 
-    image_list[0].save('export.gif', format = 'GIF', save_all = True, loop = 0, append_images = image_list)
-    
 
-    
-    ## Download ##
-    st.subheader('Download')
-    
 
-    
-    file_ = open('export.gif', 'rb')
-    contents = file_.read()
-    data_url = base64.b64encode(contents).decode("utf-8")
-    file_.close()
-    st.markdown(
-        f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
-        unsafe_allow_html=True,
-    )
-    
-    fsize = round(os.path.getsize('export.gif')/(1024*1024), 1)
-    st.info(f'File size of generated GIF: {fsize} MB', icon='💾')
-    
-    fname = uploaded_file.name.split('.')[0]
-    with open('export.gif', 'rb') as file:
-      btn = st.download_button(
-            label='Download image',
-            data=file,
-            file_name=f'{fname}_scaling-{selected_resolution_scaling}_fps-{st.session_state.clip_fps}_speed-{selected_speedx}_duration-{selected_export_range[0]}-{selected_export_range[1]}.gif',
-            mime='image/gif'
-          )
 
-## Default page ##
-else:
-  st.warning('👈 Upload your video file')
+if ('clip' in st.session_state and 
+    'selected_export_range' in st.session_state and 
+    'selected_speedx' in st.session_state and 
+    'selected_resolution_scaling' in st.session_state):
+  with st.form('gen_gif'):
+    ## Export animated GIF ##
+      st.subheader('Generate GIF')
+      submitted = st.form_submit_button('Generate Animated GIF')
+      if submitted:
+          st.session_state.clip = tmp_clip
+          generate_gif()
+
 
 
